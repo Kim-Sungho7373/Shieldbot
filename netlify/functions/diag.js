2891d447f3120e8cd71372244f3fdec3392d078b
Kim-Sungho <sungho7373@naver.com>
2025-08-28 20:29:08 +0900

    fix(diag): correct content-type and add log

--- netlify/functions/diag.js
+++ netlify/functions/diag.js
@@ -1,4 +1,6 @@
+// netlify/functions/diag.js
 export async function handler() {
+  console.log("diag invoked, env key present?:", Boolean(process.env.OPENAI_API_KEY));
   return {
     statusCode: 200,
     headers: { "content-type": "application/json" },
